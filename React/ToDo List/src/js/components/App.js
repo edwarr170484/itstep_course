93765664aa42aac742c457b697e6186346137412
Edgar Zhinevskij <edwarr170484@gmail.com>
2026-02-16 21:30:46 +0300

    Добавил окончательный вариант TODO list на React

--- React/ToDo List/src/js/components/App.js
+++ React/ToDo List/src/js/components/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 import H1 from './headers/H1';
 
@@ -7,7 +7,9 @@ function uid() {
 }
 
 export default function App() {
-    const [tasks, setTasks] = useState([]);
+    const savedTasks = window.localStorage.getItem("tasks");
+
+    const [tasks, setTasks] = useState(savedTasks ? JSON.parse(savedTasks) : []);
     const [text, setText] = useState("");
 
     function addNewTask(event) {
@@ -15,33 +17,64 @@ export default function App() {
 
         const newTasks = [
             ...tasks,
-            {id: uid(), text: text, status: 0}
+            {id: uid(), text: text, status: false}
         ];
 
         setTasks(newTasks);
+
+        setText("");
     }
+
+    function changeStatus(taskId) {
+        const newTasks = tasks.map((task) => {
+            if(task.id == taskId) {
+                task.status = !task.status;
+            }
+
+            return task;
+        });
+
+        setTasks(newTasks);
+    }
+
+    function deleteTask(taskId){
+        if(confirm('Вы действительно хотите удалить эту задачу?')) {
+            const newTasks = tasks.filter((task) => {
+                return task.id != taskId;
+            });
+
+            setTasks(newTasks);
+        }
+    }
+
+    useEffect(() => {
+        window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
     
     return (
         <>
             <H1>ToDo</H1>
-            <form class="form" onSubmit={addNewTask}>
-                <input type="text" placeholder="Новая задача..." onChange={ (e) => setText(e.target.value) } />
+            <form className="form" onSubmit={addNewTask}>
+                <input type="text" placeholder="Новая задача..." value={text} onChange={ (e) => setText(e.target.value) } />
                 <button type="submit">Добавить</button>
             </form>
-            <ul class="list">
-                {
-                    tasks.map(function(task){
-                        return (
-                                <li class="item">
-                                    <input type="checkbox" checked value="1" />
-                                    <span class="done">Задача 1</span>
-                                    <button>❌</button>
-                                </li>
-                            )
-                        }
-                    )
-                }
-            </ul>
+            {
+                tasks.length > 0 ? 
+                <ul className="list">
+                    {
+                        tasks.map(function(task){
+                            return (
+                                    <li className="item" key={task.id}>
+                                        <input type="checkbox" checked={task.status} value="1" onChange={ () => {changeStatus(task.id)}} />
+                                        <span className={task.status ? "done" : null}>{task.text}</span>
+                                        <button onClick={() => {deleteTask(task.id)}}>❌</button>
+                                    </li>
+                                )
+                            }
+                        )
+                    }
+                </ul> : "Список задач пустой"
+            }
         </>
     );
 }
